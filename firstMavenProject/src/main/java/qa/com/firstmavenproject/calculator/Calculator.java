0d0067acb547384624e5501c54ebe7e81b23fabd
Sam Picco Tarullo <68030022+starullo@users.noreply.github.com>
2021-05-25 16:27:48 -0500

    ST: added exception handler for division method to handle zeros

--- firstMavenProject/src/main/java/qa/com/firstmavenproject/calculator/Calculator.java
+++ firstMavenProject/src/main/java/qa/com/firstmavenproject/calculator/Calculator.java
@@ -14,7 +14,12 @@ public class Calculator {
 		return a * b;
 	}
 	
-	public static double divide(int a, int b) {
+	public static double divide(int a, int b) throws ArithmeticException {
+		if (a == 0) {
+			throw new ArithmeticException("Cannot divide zero by a number");
+		} else if (b == 0) {
+			throw new ArithmeticException("Cannot divide a number by zero");
+		}
 		return a / b;
 	}
 	
